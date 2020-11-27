59ff8196f3a9dec046bb2af80ddc3ac50417613c
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.station>
2020-11-27 17:53:01 +0100

    test updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_config/flutter_config.dart';
 
 import 'package:fl_notes/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  FlutterConfig.loadValueForTesting({'ENV': 'MOCK'});
+
+  testWidgets('Render main widget', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the main widget renders.
+    expect(find.text('Hello World MOCK'), findsOneWidget);
   });
 }
