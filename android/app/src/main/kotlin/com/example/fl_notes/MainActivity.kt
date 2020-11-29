9494c32c1e0240a98496e2fbf22cee28b8b1ced1
Tommaso Sebastianelli <tommaso-sebastianelli@users.noreply.github.com>
2020-11-29 13:16:39 +0100

    Release/0.9.0 1 (#7)

---
+++ android/app/src/main/kotlin/com/example/fl_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fl_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,24 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+import 'package:fl_notes/main.dart';
+
+void main() {
+  FlutterConfig.loadValueForTesting({'ENV': 'MOCK'});
+
+  testWidgets('Render main widget', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify the main widget renders.
+    expect(find.text('env: MOCK'), findsOneWidget);
+  });
+}
