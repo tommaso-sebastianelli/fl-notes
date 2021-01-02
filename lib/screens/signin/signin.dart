1a0c8f7bef6a6e15fc946ea0670be5a0f8a4177c
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MacBook-Pro.local>
2021-01-02 12:12:15 +0100

    UI signIn and navigator flow

---
+++ lib/screens/signin/signin.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(FlutterConfig.get('LABEL')),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GoogleSignInButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/board');
+              },
+              darkMode: false, // default: false
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
