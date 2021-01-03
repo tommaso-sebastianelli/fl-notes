d3608d3aa1e44faac61de26cb6707c90b980fefe
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MacBook-Pro.local>
2021-01-03 21:30:27 +0100

    added sign in container

--- lib/screens/signin/signin.dart
+++ lib/screens/signin/signin.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 
 class SignIn extends StatelessWidget {
@@ -8,23 +7,26 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(FlutterConfig.get('LABEL')),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GoogleSignInButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/board');
-              },
-              darkMode: false, // default: false
-            ),
-          ],
+    return WidgetsApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(FlutterConfig.get('LABEL')),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GoogleSignInButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/board');
+                },
+                darkMode: false, // default: false
+              ),
+            ],
+          ),
         ),
       ),
+      color: null,
     );
   }
 }
