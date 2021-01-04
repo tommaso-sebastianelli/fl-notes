cb4d2d88d24e6b0226c19b88452c9480f9b9853f
Tommaso Sebastianelli <tommaso-sebastianelli@users.noreply.github.com>
2021-01-04 12:17:50 +0100

    PR for feature/UILogin  (#19)

---
+++ lib/screens/board/board.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Board extends StatefulWidget {
+  Board({Key key}) : super(key: key);
+
+  @override
+  _BoardState createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
+    );
+  }
+}
---
+++ lib/screens/signin/signin.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(FlutterConfig.get('LABEL')),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GoogleSignInButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/board');
+                },
+                darkMode: false, // default: false
+              ),
+            ],
+          ),
+        ),
+      ),
+      color: null,
+    );
+  }
+}
