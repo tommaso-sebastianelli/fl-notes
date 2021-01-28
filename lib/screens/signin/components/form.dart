df4b182bd513e8f588639d8043355eda6b340b60
Tommaso Sebastianelli <tommaso.sebastianelli@mikamai.com>
2021-01-28 16:41:39 +0100

    code linting

--- lib/screens/signin/components/form.dart
+++ lib/screens/signin/components/form.dart
@@ -2,13 +2,15 @@ import 'package:fl_notes/blocs/authentication.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SignInForm extends StatelessWidget {
   const SignInForm({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+        Widget>[
       Expanded(
         flex: 3,
         child: Column(
@@ -39,13 +41,12 @@ class SignInForm extends StatelessWidget {
                       ),
 
                       // Icon(Icons.login),
-                      const Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-                        child: Text('A note app written with Flutter',
-                            maxLines: 2,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 100, vertical: 10),
+                        child: Text(AppLocalizations.of(context).signInAppDesc,
                             textAlign: TextAlign.center,
-                            style: TextStyle(
+                            style: const TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.w600,
                               color: Colors.white,
