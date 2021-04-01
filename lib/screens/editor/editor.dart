a86b08efbe698c7a9fbefc7b2af3d213eab09549
Tommaso Sebastianelli <tommaso.sebastianelli@mikamai.com>
2021-04-01 15:46:52 +0200

    created editor header component

--- lib/screens/editor/editor.dart
+++ lib/screens/editor/editor.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 
 import 'package:fl_notes/blocs/notes.dart';
 import 'package:fl_notes/models/note.dart';
+import 'package:fl_notes/screens/editor/editorHeader/editor_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class Editor extends StatefulWidget {
@@ -93,50 +93,7 @@ class _EditorState extends State<Editor> {
         child: Padding(
           padding: const EdgeInsets.fromLTRB(22, 4, 22, 8),
           child: Column(children: [
-            Flexible(
-                flex: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    BlocBuilder<NotesBloc, NotesState>(
-                        builder: (BuildContext context, NotesState state) {
-                      if (state.saving) {
-                        return Text(AppLocalizations.of(context)
-                            .editorSaving
-                            .toString());
-                      } else if (state?.editingNote?.created != null) {
-                        return Text(
-                          AppLocalizations.of(context)
-                                  .editorLastSaved
-                                  .toString() +
-                              DateFormat(' dd/MM/yy, H:m:s').format(
-                                  state.editingNote?.edited ??
-                                      state.editingNote?.created),
-                          style: const TextStyle(
-                              color: Colors.black38, decorationThickness: 5.0),
-                        );
-                      } else {
-                        return const Text('');
-                      }
-                    }),
-                    BlocBuilder<NotesBloc, NotesState>(
-                        builder: (BuildContext context, NotesState state) {
-                      if (state.editingNote?.created != null) {
-                        return TextButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: Text(AppLocalizations.of(context)
-                                .genericDelete
-                                .toString()));
-                      } else {
-                        return TextButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: Text(AppLocalizations.of(context)
-                                .genericCancel
-                                .toString()));
-                      }
-                    }),
-                  ],
-                )),
+            const Flexible(flex: 0, child: EditorHeader()),
             Flexible(
               flex: 0,
               child: Padding(
