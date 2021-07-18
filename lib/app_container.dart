4023dbcb95cff59a138c946ea17890bc98b258a9
tommaso-sebastianelli <tommasosebastianelli@gmail.com>
2021-07-18 23:26:38 +0200

    fix

--- lib/app_container.dart
+++ lib/app_container.dart
@@ -4,7 +4,6 @@ import 'package:fl_notes/screens/editor/editor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'screens/board/board.dart';
 import 'screens/signin/signin.dart';
